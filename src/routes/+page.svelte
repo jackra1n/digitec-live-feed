110a597b86b0c449f567ad3e838ae64e29c30805
jackra1n <lok7ok@gmail.com>
2023-10-14 23:25:19 +0200

    WIP - getting live feed data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    export let data;
+
+    let socialShopping = data.data[0].data.socialShopping;
+    let live_feed_items = socialShopping.items;
+    console.log(live_feed_items);
+    
+</script>
+
+<div class="h-screen flex justify-center items-center">
+    <div class="border-2 border-sky-200 p-2">
+        <h1 class="text-2xl">Itmes:</h1>
+        <div>
+            <p>
+                {live_feed_items}
+            </p>
+
+        </div>
+
+    </div>
+</div>
+
