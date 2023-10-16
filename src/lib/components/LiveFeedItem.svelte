a82d58125fa96d8b8ca358161985822571534c1a
jackra1n <lok7ok@gmail.com>
2023-10-16 10:54:25 +0200

    Create LiveFeedItem component

---
+++ src/lib/components/LiveFeedItem.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let item = {};
+</script>
+
+<div class="flex border-2 border-sky-200 p-2 mt-2 rounded-md bg-sky-200">
+    <img src={item.imageUrl} alt={item.fullProductName} class="w-16 h-16 p-1 rounded-md mr-4 object-contain bg-white"/>
+    <div>
+        <p><strong>{item.userName}</strong> from <strong>{item.cityName}</strong> bought</p>
+        {#if item.brandName != null}
+            <p>{item.brandName} - {item.fullProductName}</p>
+        {:else}
+            <p>{item.fullProductName}</p>
+        {/if}
+        {#if item.salesPrice != null}
+            <p>CHF {item.salesPrice.amountIncl}</p>
+        {/if}
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
     import { onMount } from 'svelte';
+    import LiveFeedItem from '$lib/components/LiveFeedItem.svelte';
 
     /**
 	 * @type {any[]}
@@ -36,21 +37,8 @@
             <p>Refresh in: {countdown}</p>
         </div>
         <div class="max-h-min overflow-y-auto">
-            {#each live_feed_entries as entry}
-            <div class="flex border-2 border-sky-200 p-2 mt-2 rounded-md bg-sky-200">
-                <img src={entry.imageUrl} alt={entry.fullProductName} class="w-16 h-16 p-1 rounded-md mr-4 object-contain bg-white"/>
-                <div>
-                    <p><strong>{entry.userName}</strong> from <strong>{entry.cityName}</strong> bought</p>
-                    {#if entry.brandName != null}
-                        <p>{entry.brandName} - {entry.fullProductName}</p>
-                    {:else}
-                        <p>{entry.fullProductName}</p>
-                    {/if}
-                    {#if entry.salesPrice != null}
-                        <p>CHF {entry.salesPrice.amountIncl}</p>
-                    {/if}
-                </div>
-            </div>
+            {#each live_feed_entries as item}
+                <LiveFeedItem {item} />
             {/each}
         </div>
 
