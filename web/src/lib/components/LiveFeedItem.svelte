b5a3e33c786a0a887159b05cf18e1dda56a2c529
jackra1n <lok7ok@gmail.com>
2023-10-17 22:15:03 +0200

    Web add more feed item types

--- web/src/lib/components/LiveFeedItem.svelte
+++ web/src/lib/components/LiveFeedItem.svelte
@@ -14,27 +14,25 @@
         <p>{item.brandName} {item.fullProductName}</p>
     </div>
 
-    {:else if [2,3].includes(item.socialShoppingTransactionTypeId)}
+    {:else if item.socialShoppingTransactionTypeId == 2}
 
     <p>{item.userName} from {item.cityName} registered as a new user</p>
 
-    {:else if item.socialShoppingTransactionTypeId == 5}
-
-    <img src={item.imageUrl} alt={item.fullProductName} class="w-16 h-16 p-1 rounded-md mr-4 object-contain bg-white">
-    <div>
-        <p>{item.brandName} {item.fullProductName}</p>
-        <p><strong>was shipped to</strong> {item.userName}</p>
-    </div>
-
-    {:else}
+    {:else if [3, 4].includes(item.socialShoppingTransactionTypeId)}
 
     <img src={item.imageUrl} alt={item.fullProductName} class="w-16 h-16 p-1 rounded-md mr-4 object-contain bg-white"/>
     <div>
         <p>
             <strong>{item.userName}</strong> 
             {#if item.cityName != null}
-            from <strong>{item.cityName}</strong> bought
+                from <strong>{item.cityName}</strong> 
             {/if} 
+
+            {#if item.socialShoppingTransactionTypeId == 3}
+                ordered
+            {:else}
+                picked up
+            {/if}
         </p>
         {#if item.brandName != null}
         <p>{item.brandName} {item.fullProductName}</p>
@@ -46,6 +44,23 @@
         <p>CHF {item.salesPrice.amountIncl}</p>
         {/if}
     </div>
+
+    {:else if item.socialShoppingTransactionTypeId == 5}
+
+    <img src={item.imageUrl} alt={item.fullProductName} class="w-16 h-16 p-1 rounded-md mr-4 object-contain bg-white">
+    <div>
+        <p>{item.brandName} {item.fullProductName}</p>
+        <p><strong>was shipped to</strong> {item.userName}</p>
+    </div>
+
+    {:else if item.socialShoppingTransactionTypeId == 19}
+
+    <p>{item.userName} rated {item.brandName} {item.fullProductName} with {item.rating} stars</p>
+
+    {:else}
+
+    <p>Unknown transaction type: {item.socialShoppingTransactionTypeId}</p>
+
         
     {/if}
     </div>
