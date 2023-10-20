3ad02b08426cb8ceaa04b9ad37ec6d492ce5cc0c
jackra1n <lok7ok@gmail.com>
2023-10-21 00:20:27 +0200

    Move from Bun to Nodejs
    Bun is unfortunately still too buggy to deploy. Maybe next year.

--- server/README.md
+++ server/README.md
@@ -1,7 +1,9 @@
-```
-bun install
-bun run dev
-```
+
+1. `npm install`
+2. Create a .env file in the root of the server directory (see example.env for reference)
+3. `npx prisma generate`
+4. `npm run start`
+
 
 ```
 open http://localhost:3000
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,23 +1,12 @@
-// @ts-check
-const { skeleton } = require('@skeletonlabs/tw-plugin');
+/** @type {import('tailwindcss').Config}*/
+const config = {
+  content: ["./src/**/*.{html,js,svelte,ts}"],
 
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-	darkMode: 'class',
-	content: [
-		'./src/**/*.{html,js,svelte,ts}',
-		require('path').join(require.resolve(
-			'@skeletonlabs/skeleton'),
-			'../**/*.{html,js,svelte,ts}'
-		)
-	],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		skeleton({
-			themes: { preset: [ "skeleton" ] }
-		})
-	]
-}
-						
+  theme: {
+    extend: {},
+  },
+
+  plugins: [],
+};
+
+module.exports = config;
